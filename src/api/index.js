834f26114f64615d46192f9aa258612f2277623f
Shakhzod Akhmadov <axmedovshaxzod14@gmail.com>
2023-02-18 09:17:04 +0500

    build-dep

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ const apiUrl = axios.create({
   headers: {
     Authorization:
       "Bearer BQBlYyG-yh7Gquz-4Yg1O4t_2FXiJIt37dRuLWnd1QRy0MIYMjOXfSPLHxSps8jZ3OWu7yKIkNEKUdgUY5_7XQfJayRvTdOHFjbLaVXe6lfJPYyFJOFbnTKMO4h9PJaI5QhIsm9zki4s7U2iLRc4nSPS-5Sq7hih1H-ibAISK58_-0fSpQR8eL8DiTdcoNMru7iFBOFi3m8hKf9gDtSB5gBSPm6Rtm4gAHxGnf2pO07Mkqo8rRhxTjCHqIQAn66q5yz89-PLpYCGsquYo7RnPrM",
-    Host: "api.spotify.com",
+    // Host: "api.spotify.com",
   },
 });
 
